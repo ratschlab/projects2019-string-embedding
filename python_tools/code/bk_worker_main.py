0862ee4a0bf628d63daa2513597b0378e0e446ef
ajoudaki <ajoudaki@inf.ethz.ch>
2019-06-10 20:48:26 +0200

    debugging

--- python_tools/code/bk_worker_main.py
+++ python_tools/code/bk_worker_main.py
@@ -58,14 +58,54 @@ def load_files(file_name, clean):
     return seqs, vals, num_seqs, options, Op, paths
     
 
+def get_job_path(job, job_paths):
+    S = job_paths[job[0]] + job[0]
+    for i in range(1,len(job)):
+        S = S + '_' + str(job[i])
+    return S + '.done'
+
+def get_job_cmd(job):
+    S = ' -t ' + job[0] 
+    argnames = ['', ' -i ', ' -j '] 
+    for i in range(1,len(job)):
+        S = S + argnames[i] + str(job[i])
+    return S 
+
+def add_job(Map, job, deps):
+    Map[job] = deps
+
+def crawl_dep_recurse(Map, Set, curr):
+    if curr not in Set:
+        Set.add(curr)
+        if curr in Map.keys():
+            for job in Map[curr]:
+                crawl_dep(Map, Set, job)
+
+def crawl_dep(Map, curr):
+    Set = set()
+    crawl_dep_recurse(Map, Set,curr)
+    return list(Set)
+
+
+def run_jobs(jobs_flat, job_dep, command, job_paths):
+    for job in jobs_flat:
+        jp = get_job_path(job,job_paths)
+        if not os.exists(jp):
+            deps_satisfied = [os.exists(get_job_path(j,job_paths)) for j in job_dep[job] ] 
+            if all(deps_satisfied):
+                cmd_combo = 'eval " ' + command + get_job_cmd(job) + '; echo dummy > ' + jp + ' " ' 
+                print('running command: '+ cmd_combo)
+                os.system(cmd_combo)
+
+
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-f','--file-name',dest='file_name', default = 'seqs0', type='string', help = 'seq file to load')
     parser.add_option('-i','--seq-id',dest='seq_id', default = 0, type='int', help = 'seq id to build index for')
     parser.add_option('-j','--search-seq-id',dest='search_seq_id', default = 0, type='int', help = 'search sequence id')
-    parser.add_option('-k','--small-k',dest='k_small', default = 3, type='int', help = 'small k to do kmer counting')
-    parser.add_option('-K','--big-k',dest='k_big', default = 100, type='int', help = 'big K to slide over the strings')
+    parser.add_option('-k','--small-k',dest='small_k', default = 3, type='int', help = 'small k to do kmer counting')
+    parser.add_option('-K','--big-k',dest='big_k', default = 100, type='int', help = 'big K to slide over the strings')
     parser.add_option('-D','--proj-dim',dest='proj_dim', default = 120, type='int', help = 'number of dimensions to project onto')
     parser.add_option('-n','--num-trees',dest='num_trees', default = 2, type='int', help = 'number of trees to be built')
     parser.add_option('-s','--step-build',dest='step_build', default = 5, type='int', help = 'step for indices of KNN built')
@@ -74,14 +114,14 @@ if __name__ == '__main__':
     parser.add_option('-N','--num-neighbors',dest='num_neighbors', default = 50, type='int', help = 'number of nearest neighbors to search')
     parser.add_option('-M','--memory',dest='memory', default = 30, type='int', help = 'memory to allocate for each process')
     parser.add_option('-t','--target',dest='target', default = 'build', type='string', help = 'metric to be used')
-    parser.add_option('-T','--forward-target',dest='forward_target', default = 'all', type='string', help = 'metric to be used')
+    parser.add_option('-T','--forward-target',dest='forward_target', default = 'merge', type='string', help = 'metric to be used')
     (options, args) = parser.parse_args()
 
     sid = options.seq_id
     sid2 = options.search_seq_id
     proj_dim = options.proj_dim
-    k_small = options.k_small 
-    k_big = options.k_big
+    k_small = options.small_k 
+    k_big = options.big_k
     num_trees = options.num_trees
     step_build = options.step_build
     step_search = options.step_search
@@ -89,12 +129,64 @@ if __name__ == '__main__':
     metric = options.metric
 
 
-    if option.target=='all':
-        pass
+    if options.target=='all':
+        result_path, index_path, search_path, eval_path, npz_path = load_paths(options.file_name)
+        summary = np.load(result_path+'num.npz')
+        N = summary['N']
+        jobs_dep = dict()
+        add_job(jobs_dep,('clean'),[])
+        build_jobs = []
+        for i in range(N):
+            job = ('build',i) 
+            add_job(jobs_dep,job,[('clean')])
+            build_jobs.append(job)
+        searh_jobs = []
+        for i in range(N):
+            for j in range(N):
+                if i!=j:
+                    job = ('search',i,j)
+                    add_job( jobs_dep,job,[('build',i),('build',j)]  )
+                    search_jobs.append(search_jobs)
+        eval_jobs = []
+        for i in range(N):
+            for j in range(N):
+                if i!=j:
+                    job = ('eval',i,j)
+                    add_job( jobs_dep, job, [ ('search',i,j) ]  )
+                    eval_jobs.append(job)
+        add_job(jobs_dep, ('merge'), eval_jobs)
 
+        job_paths = {'clean':result_path, 
+                'build': index_path,
+                'search' : search_path,
+                'eval': eval_path,
+                'merge': result_path }
+        dep_job = {'clean':[], 
+                'build': [('clean')],
+                'search' : build_jobs,
+                'eval': search_jobs,
+                'merge': eval_jobs }
+        add_job(jobs_dep,('empty'), dep_job[optioins.forward_target]) 
+        jobs_flat = crawl_dep(jobs_dep)
+        command = 'bsub -R "rmemusage[' + str(options.memory) + '000]"  python bk_worker_main.py '
+
+        for k,v in options.__dict__.items():
+            if k not in ['target', 'seq_id', 'search_seq_id', 'target', 'forward_target']:
+                command = command + '--' + k.replace('_','-') + ' ' + str(v) 
+        run_jobs(jobs_flat, jobs_dep, command, job_paths) 
+
+    elif options.target=='empty':
+        pass
+        
+    elif options.target=='merge':
+        print('merge called')
+        pass
 
     elif options.target=='clean':
         load_files(options.file_name, clean=True)
+        result_path, index_path, search_path, eval_path, npz_path = load_paths(options.file_name)
+        seqs, vals, num_seqs, opts, Op, paths = load_files(options.file_name, clean=False)
+        np.savez(result_path+'num.npz', N=len(seqs), num_seqs = num_seqs, options = options)
 
     elif options.target=='build':
     
@@ -176,8 +268,9 @@ if __name__ == '__main__':
                 quadrupples = quadrupples, 
                 total_count = total_count, 
                 total_correct = total_correct)
-        #np.savez(eval_path +str(sid)+'_'+str(sid2)+'.done', sid=sid)
+        np.savez(eval_path +str(sid)+'_'+str(sid2)+'.done', sid=sid)
         print('done',file=open(eval_path +str(sid)+'_'+str(sid2)+'.done','w+'))
+
         print('false positive = ', (total_count - total_correct)/total_count)
         print('recall = ', len(quadrupples)*1.0/num_genes)
 
