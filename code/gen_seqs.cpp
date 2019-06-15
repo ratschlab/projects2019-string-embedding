0267c54cc61329bfec41e08901d350294290d6a5
ajoudaki <ajoudaki@inf.ethz.ch>
2019-06-15 05:45:58 +0200

    debugged sequence generation

--- code/gen_seqs.cpp
+++ code/gen_seqs.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <random>
 #include <vector>
+#include <tuple>
 using namespace std;
 
 
@@ -51,6 +52,9 @@ public:
             if (check_arg(argv[i],"-Y", "--num-seq2")) {
                 num_seq2 = stoi(argv[++i]);
             } else 
+            if (check_arg(argv[i],"-Z", "--geometric-p")) {
+                geometric_p = stof(argv[++i]);
+            } else 
             if (check_arg(argv[i],"-c", "--colored")) {
                 colored = true;
             } else 
@@ -74,6 +78,11 @@ public:
     }
 };
 
+std::string vec2str(std::vector<char> &vec) {
+    std::string str(vec.begin(), vec.end());
+    return str;
+}
+
 
 int rand_int(int min, int max) {
     int rv = std::rand();
@@ -91,20 +100,27 @@ vector<int> rand_vect(int min, int max, int size) {
     return vec;
 }
 
-vector<char> rand_seq(string Sigma, int len) {
+
+
+void rand_seq(vector<char> &seq,string Sigma, int len) {
     int sigma = Sigma.length();
-    vector<char> seq(len);
     for (int i=0; i<len; i++) 
-        seq[i] = Sigma[rand_int(sigma)];
-    return seq;
+        seq.push_back( Sigma[rand_int(sigma)] );
+}
+
+vector<char> rand_seq(std::string Sigma, int len) {
+    std::vector<char> vec;
+    vec.reserve(len);
+    rand_seq(vec, Sigma, len);
+    return vec;
 }
 
 
-vector<char> mutate_seq(vector<char> &seq, seq_options &opts, std::string Sigma) {
+int  mutate_seq(std::vector<char> &seq, std::vector<char> &seq2, seq_options &opts, std::string Sigma) {
     std::default_random_engine gen;
     std::geometric_distribution<int> db(opts.geometric_p);
-    vector<char> seq2;
     int i = 0;
+    int init_size = seq2.size();
     while (i< seq.size()) {
         double rv = (double)rand() / RAND_MAX;
         if (rv< opts.mutation_rate) {
@@ -135,47 +151,84 @@ vector<char> mutate_seq(vector<char> &seq, seq_options &opts, std::string Sigma)
             i++;
         }
     }
-    string s(seq2.begin(), seq2.end());
-    return seq2;
+    return seq2.size() - init_size;
 }
 
-void print_seq(vector<char> &vec) {
-    for (int i; i<vec.size(); i++)
-        cout << vec[i];
-    cout << endl;
+std::vector<char> mutate_seq(std::vector<char> &seq, seq_options &opts, std::string Sigma) {
+    vector<char> seq2;
+    mutate_seq(seq, seq2, opts, Sigma);
 }
 
 
+struct gene_interval {
+public:
+    int gene_code, begin, end;
+    gene_interval(int g, int b, int e ) : gene_code(g), begin(b), end(e) {}
+};
 
-void gen_seqs(seq_options opt) {
+
+void generate_seqs(vector<vector<char>>  &seqs, vector<vector<int> > &vals, vector<vector< gene_interval> > &all_intervals, seq_options &opt) {
     string Sigma = "acgt";
     auto gene_lens = rand_vect(opt.gene_len, opt.gene_len2+1, opt.num_genes);
     auto num_seqs = rand_vect(opt.num_seq, opt.num_seq2+1, opt.repeat);
+    
+    for (int ri=0; ri< opt.repeat; ri++) {
+        vector<vector<char> > genes;
+        for (int k=0; k<opt.num_genes; k++) {
+            genes.push_back(rand_seq(Sigma, gene_lens[k]));
+        }
+        for (int i=0; i<num_seqs[ri]; i++) {
+            vector<char> seq;
+            vector<int> val;
+            vector<gene_interval> gene_interval_vec; 
+            for (int k=0; k<opt.num_genes; k++) {
+                int gene_code = (ri*opt.num_genes) + k + 1;
+                rand_seq(seq, Sigma, opt.padding);
+                val.insert(val.end(), opt.padding, 0); 
+                int gene_len;
+                int begin = seq.size();
+                if (i==0) {
+                    seq.insert(seq.end(), genes[k].begin(), genes[k].end());
+                    gene_len = gene_lens[k];
+                } else {
+                    gene_len = mutate_seq(genes[k], seq,  opt, Sigma); 
+                }
+                int end = seq.size();
+                gene_interval_vec.push_back(gene_interval(gene_code, begin, end));
+                val.insert(val.end(), gene_len, gene_code);
+                rand_seq(seq, Sigma, opt.padding);
+                val.insert(val.end(), opt.padding, 0); 
+
+            }
+            seqs.push_back(seq);
+            vals.push_back(val);
+            all_intervals.push_back(gene_interval_vec);
+        }
+
+    }
 }
+
 
 int main(int argc, char* argv[] ) 
 {
     std::srand(std::time(nullptr)); // use current time as seed for random generator
     seq_options opt; 
     opt.read_args(argc, argv);
-    int len = 100;
-    string Sigma = "acgt";
-    auto seq = rand_seq(Sigma, len);
-    auto seq2 = mutate_seq(seq, opt, Sigma);
-    string s1(seq.begin(), seq.end());
-    string s2(seq2.begin(), seq2.end());
-    cout << " seq1 = " << s1 << endl;
-    cout << " seq2 = " << s2 << endl;
+    vector<vector<gene_interval> > all_intervals;
+    vector<vector<char> > seqs;
+    vector<vector<int> > vals;
+    generate_seqs(seqs, vals, all_intervals, opt);
 
-    /*
-    cout << "opt.mutation_rate = " <<  opt.mutation_rate << endl; 
-    cout << " save dir = " << opt.save_directory << endl;
-    for (int i = 0; i<5; i++) {
-        int random_variable = std::rand();
-        cout << "rand int = " << random_variable << endl;
-        double f = (double)rand() / RAND_MAX;
-        cout << "rand double = " << f << endl; 
+    for (int i=0; i<seqs.size() ; i++) {
+        auto str = vec2str(seqs[i]);
+  //      cout << " seq "  << i <<  " : " << endl;
+        cout << str << endl;
+  //      for (int j=0; j<vals[i].size(); j++)
+  //          if (vals[i][j]>0)
+ //               cout << vals[i][j];
+                //cout << str[i][j];
+//        cout << endl;
     }
-    */
 
+    return 0;
 }
